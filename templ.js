6024eeb2da6663ddeaeb001d9d593ed72bed0290
Ilya Basiuk <ilyabasiuk@gmail.com>
2014-11-21 07:50:17 -0500

    shanged image

--- templ.js
+++ templ.js
@@ -4,7 +4,9 @@
 
 
           var myShape = document.getElementById("myElementToAnimate"),
-              animateMotion = document.createElementNS("http://www.w3.org/2000/svg", "animateMotion");
+              animateMotion =
+                document.createElementNS("http://www.w3.org/2000/svg",
+                                         "animateMotion");
 
               animateMotion.setAttribute("dur", "8s");
               animateMotion.setAttribute("d", "M 0 0 L 100 100");
@@ -17,12 +19,14 @@
 
 
 
-              animateMotion.addEventListener("repeatEvent", function() {
-                  console.log("new iteration");
+              animateMotion.addEventListener("beginEvent", function() {
+                  console.log("start of animation");
               });
+
               animateMotion.addEventListener("endEvent", function() {
                   console.log("end of animation");
               });
-              animateMotion.addEventListener("beginEvent", function() {
-                  console.log("start of animation");
+
+              animateMotion.addEventListener("repeatEvent", function() {
+                  console.log("new iteration");
               });

49460bb682573d8967a208fd98da01f6e48d6bc0
Ilya Basiuk <ilyabasiuk@gmail.com>
2014-11-21 07:36:49 -0500

    added event listeners slide for smil animation

---
+++ templ.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+          var myShape = document.getElementById("myElementToAnimate"),
+              animateMotion = document.createElementNS("http://www.w3.org/2000/svg", "animateMotion");
+
+              animateMotion.setAttribute("dur", "8s");
+              animateMotion.setAttribute("d", "M 0 0 L 100 100");
+              animateMotion.setAttribute("repeatCount", "indefinite");
+              animateMotion.setAttribute("begin", "indefinite");
+
+              myShape.appendChild(animateMotion);
+              animateMotion.beginElement();
+
+
+
+
+              animateMotion.addEventListener("repeatEvent", function() {
+                  console.log("new iteration");
+              });
+              animateMotion.addEventListener("endEvent", function() {
+                  console.log("end of animation");
+              });
+              animateMotion.addEventListener("beginEvent", function() {
+                  console.log("start of animation");
+              });
